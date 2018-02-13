e6962fba94e87706c221e8da574c7377bf449f42
Tyler Jankowski <35941543+TJank@users.noreply.github.com>
2018-02-13 12:44:57 -0600

    Work on ch3 book exercises

---
+++ Chapter3/ch3BookExercises.py
@@ -0,0 +1,75 @@
+# PG 89
+name = "Tyler Jonathan Jankowski"
+firstname = name[0:5]
+lastname = name[15:len(name) +1]
+print(lastname + ", " + firstname)
+print(len(firstname))
+
+s = "s"
+p = "p"
+
+mississippi = "mi" + s + s + "i" + s + s + "i" + p + p + "i"
+#print(mississippi)
+
+name2 = "Roy G Biv"
+#for i in range(len(name2) + 1):
+    #print(name2[0:i])
+
+# PG 91
+mis = "mississippi"
+#print(mis.count("s"))
+#print(mis.replace("iss", "ox"))
+#print(mis.index("p"))
+p = "python"
+p = p.upper()
+#print(p.center(20))
+
+# Listing 3.1 PG 93
+def letterToIndex(ch):
+    alphabet = "abcdefghijklmnopqrstuvwxyz "
+    idx = alphabet.find(ch)
+    if idx < 0:
+        print("error: letter not in the alphabet", ch)
+    return idx
+
+def indexToLetter(idx):
+    alphabet = "abcdefghijklmnopqrstuvwxyz "
+    if idx > 25:
+        print("error: ", idx, " is too large.")
+        letter = ""
+    elif idx < 0:
+        print("error: ", idx, " is less than 0.")
+        letter = ""
+    else:
+        letter = alphabet[idx]
+    return letter
+
+#print(letterToIndex("t"))
+#print(indexToLetter(19))
+
+# PG 93 Exercises
+#print(ord('a'))
+#print(ord('A'))
+def characterToIntValue(ch):
+    print(ord(ch))
+
+#characterToIntValue('A')
+
+# Listing 3.2 PG 95
+def scramble2Encrypt(plainText):
+    evenChars = ""
+    oddChars = ""
+    charCount = 0
+    for ch in plainText:
+        if charCount % 2 == 0:
+            evenChars = evenChars + ch
+        else:
+            oddChars = oddChars + ch
+        charCount = charCount + 1
+    cipherText = oddChars + evenChars
+    return cipherText
+
+pText = "own noun brown cow"
+print(scramble2Encrypt(pText))
+
+print(len(pText))
