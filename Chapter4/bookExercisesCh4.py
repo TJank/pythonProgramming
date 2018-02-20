cdd06fe8076ce1b2e45f9168f832cb3bb289589a
Tyler Jankowski <35941543+TJank@users.noreply.github.com>
2018-02-20 11:39:13 -0600

    Work on ch4 book readings % exercises

--- Chapter4/bookExercisesCh4.py
+++ Chapter4/bookExercisesCh4.py
@@ -73,14 +73,97 @@ print(list)
 list.pop(2)
 print(list)
 
+# 4.3
+string = "the quick brown fox"
+listOfWords = []
+listOfWords.append(string[0:3])
+listOfWords.append(string[4:9])
+listOfWords.append(string[10:15])
+#print(listOfWords)
 
+mylist410 = [1,4,(1,2,3),10]
 
+print(mylist410)
 
+# Session 4.8 pg 129
+alist = [20,32,21,26,33,22,18]
+print(max(alist))
+print(min(alist))
+print(max('house'))
+print(min('house'))
+print(max(alist)- min(alist))
 
+# listing 4.1 pg 130
+def getRange(alist):
+    return max(alist) - min(alist)
 
+# Session 4.9
+print(getRange([2,4]))
+print(getRange(alist))
 
+# Listing 4.2
+def getMax(alist):
+    maxSoFar = alist[0]
+    for pos in range(1,len(alist)):
+        if alist[pos] > maxSoFar:
+            maxSoFar = alist[pos]
 
+    return maxSoFar
 
+# Exercises pg 131
+# 4.14
+def getMin(alist):
+    minSoFar = alist[0]
+    for i in range(1, len(alist)):
+        if alist[i] < minSoFar:
+            minSoFar = alist[i]
+
+    return minSoFar
+
+# 4.15
+def getMinIt(alist):
+    minSoFar = alist[0]
+    for i in alist[1:]:
+        if i < minSoFar:
+            minSoFar = i
+
+    return minSoFar
+
+print(getMinIt([1000,3,10,5,.5]))
+
+# 4.16
+def getNewRange(alist):
+    max = getMax(alist)
+    min = getMinIt(alist)
+    return max - min
+
+print(getNewRange([100,5,52,65,7,2]))
+
+# Listing 4.4 pg 131
+# MEAN
+def mean(alist):
+    mean = sum(alist) / len(alist)
+    return mean
+
+print(mean(alist))
+
+# Listing 4.5 pg 134
+def median(alist):
+    copylist = alist[:]
+    copylist.sort()
+
+    if len(copylist) % 2 == 0:      # even length
+        rightmid = len(copylist)//2
+        leftmid = rightmid - 2
+        median = (copylist[leftmid] + copylist[rightmid]) / 2
+
+    else:       # odd length
+        mid = len(copylist)//2
+        median = copylist[mid]
+
+    return median
+
+print(median(alist))
 
 
 
