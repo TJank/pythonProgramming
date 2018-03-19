263883c8d3e321a13ed9cb2a8e73a0506a19d9e6
Tyler Jankowski <35941543+TJank@users.noreply.github.com>
2018-03-19 14:48:13 -0500

    Start ch7 book exercises

---
+++ Chapter7/ch7_book_exercises.py
@@ -0,0 +1,12 @@
+# Listing 7.1 pg 240
+import math
+
+def euclidD(point1, point2):
+    total = 0
+    for index in range(len(point1)):
+        diff = (point1[index] - point2[index]) ** 2
+        total = total + diff
+
+    euclidDistance = math.sqrt(total)
+    return euclidDistance
+
