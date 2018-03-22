a5ba93221a1dabfd76a67c0fbd8bd2c6217688ff
Tyler Jankowski <35941543+TJank@users.noreply.github.com>
2018-03-22 13:34:21 -0500

    work on ch4 homework

--- Chapter4/homeWorkCh4.py
+++ Chapter4/homeWorkCh4.py
@@ -11,13 +11,13 @@ def randomList_gamma(n, a, b):
         rand = float("{0:.1f}".format(gammavariate(a, b)))
         randomList.append(rand)
     return randomList
-print("-"*20)
+
 rand_list = randomList_gamma(1000, 2, 2)
 print(rand_list)
 
 points = rand_list
 bins = int(np.max(rand_list) - np.min(rand_list))
-print(bins)
+#print(bins)
 fig, ax = plt.subplots()
 _mean = np.mean(points)
 _std = np.std(points)
@@ -69,7 +69,8 @@ listofnum = [i for i in range(11)]
 def sumList(list):
     # check if list is odd
     # git middle number
-    if not len(list) % 2 == 0:
+    midNum = 0
+    if len(list) % 2 != 0:
         midIdx = len(list) // 2
         midNum = list[midIdx]
 
@@ -88,17 +89,29 @@ def sumList(list):
 
     return sumList
 
-#print(sumList(listofnum))
+print(sumList(listofnum))
 
 # Problem 4.
 bob = turtle.Turtle()
-points = [(1,2),(5,1),(-1,5),(-5,7),(-8,-2),(-4,-1),(2,-10),(8,-3),(4,-8),(5,5)]
+points = [(1,2),(3,1),(7,9),(9,2),(5,5)]
 
 def plotRegression(points):
+    x_list = [x[0] for x in points]
+    y_list = [y[1] for y in points]
+    #print(x_list, y_list)
+    #print(sum(x_list), sum(y_list))
 
+    x_bar = sum(x_list) / len(x_list)
+    y_bar = sum(y_list) / len(y_list)
+    n = len(x_list)
 
+    m = ((sum(x_list) * sum(y_list)) - (n * x_bar * y_bar)) / (((sum(x_list))**2) - (n * (x_bar**2)))
+    #print(m)
+    #y = y_bar + (m * (x - x_bar))
+
+plotRegression(points)
 # Problem 5.
-#def gcd(x, y):
+def gcd(x, y):
     x1 = abs(min(x, y))
     y1 = abs(max(x, y))
     gcd_ = x1
@@ -107,7 +120,26 @@ def plotRegression(points):
         gcd_ = gcd(x1, y1 % x1)
     return gcd_
 
-#print(gcd(2,100))
+print(gcd(4,5))
+import fractions
+def addFractions(frac1, frac2):
+    x1, y1 = frac1[0], frac1[1]
+    x2, y2 = frac2[0], frac2[1]
+    print(fractions.Fraction(x1, y1) + fractions.Fraction(x2, y2))
+
+    # Get greatest common denominator
+    #print(gcd(y1, y2))
+
+def multiplyFractions(frac1, frac2):
+    x1, y1 = frac1[0], frac1[1]
+    x2, y2 = frac2[0], frac2[1]
+    print(fractions.Fraction(x1, y1) * fractions.Fraction(x2, y2))
+
+
+fraction1 = (1,4)
+fraction2 = (2,5)
+
+print(addFractions(fraction1, fraction2))
 
 # Problem 6.
 list1 = [i for i in range(1,10)]
@@ -125,7 +157,71 @@ print(transform(list1,list2,4,7))
 
 # Problem 7.
 
+
 # Problem 8.
+morseAlphabet = {
+    "A": ".-",
+    "B": "-...",
+    "C" : "-.-.",
+    "D" : "-..",
+    "E" : ".",
+    "F" : "..-.",
+    "G" : "--.",
+    "H" : "....",
+    "I" : "..",
+    "J" : ".---",
+    "K" : "-.-",
+    "L" : ".-..",
+    "M" : "--",
+    "N" : "-.",
+    "O" : "---",
+    "P" : ".--.",
+    "Q" : "--.-",
+    "R" : ".-.",
+    "S" : "...",
+    "T" : "-",
+    "U" : "..-",
+    "V" : "...-",
+    "W" : ".--",
+    "X" : "-..-",
+    "Y" : "-.--",
+    "Z" : "--..",
+    " " : "/"
+    }
+testCode = ".... . .-.. .-.. --- / -.. .- .. .-.. -.-- / .--. .-. --- --. .-. .- -- -- . .-. / --. --- --- -.. / .-.. ..- -.-. -.- / --- -. / - .... . / -.-. .... .- .-.. .-.. . -. --. . ... / - --- -.. .- -.-- "
+
+def getMessage(morseCode, morseAlphabet):
+    for j in morseAlphabet.values():
+
+        for i in morseAlphabet.keys():
+            if i[0]==morseCode and j == morseAlphabet:
+                if morseAlphabet[i]==j:
+                    return "i[0]"
+                print(i)
+                #return i[1]
+
+
+
+
+
+
+def fromMorseCodeToWords(morseCode, morseAlphabet):
+    chara_in_morseCode = [morseCode.split()]
+    decode_message = ""
+    print(chara_in_morseCode)
+
+    for morse_Letter in chara_in_morseCode:
+        decode_message += getMessage(morse_Letter, morseAlphabet)
+
+
+
+
+
+
+
+
+
+print(fromMorseCodeToWords(testCode, morseAlphabet))
 
 # Problem 9.
 
